b1895efefb4aed80be7ad0e47754d2c5817f9a7a
ianschwarting <ian@varasuites.com>
2026-04-30 20:53:16 -0600

    Delete sheet-sync.js

--- sheet-sync.js
+++
@@ -1,6 +0,0 @@
-import { clearSessionCookie } from '../../lib/session.js';
-
-export default function handler(req, res) {
-  res.setHeader('Set-Cookie', clearSessionCookie());
-  res.status(200).json({ ok: true });
-}
